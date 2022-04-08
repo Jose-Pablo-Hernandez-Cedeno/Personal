91f38a6c62341cbaf8ea1c41b92392d31a60cc88
Jose Pablo Hernández Cedeño <josephernandezc@gmail.com>
2022-04-08 16:32:35 -0600

    Corregido bug menú con validaciónes usando strings

--- main.cpp
+++ main.cpp
@@ -2,15 +2,63 @@
 #include <iostream>
 using namespace std;
 
-bool CinFail();
+int MenuPrincipal();//Función que imprime el menú y retorna elección del usuario
+bool ValidarCaracterNumerico(char Carcater);//Algoritmo de búsqueda lineal que retorna true solo si el caracter se valida como dato numérico
+int ConvertirChar(char Caracter);//Función que recibe un caracter numérico y retorna su quivalente en tipo int
 
 int main() {
 	setlocale(LC_CTYPE,"");
 	system("title Proyecto Final");
 
-	int EleccionMenu = 0;//Almacena la respuesta del usuario
-    bool RespuestaInvalida = true;//Boleano correspondiente al estado de invalidez de respuesta introducida
+	int EleccionMenu = 0;//Almacena la elección del usuario para el menú en caso de cumplirse parametros de entrada
+    
+	//Menú principal
+	EleccionMenu = MenuPrincipal();
+	system("cls");
 
+	//Ejecución de código según elección
+	switch (EleccionMenu) {
+	case (1):
+		//Mecánica ingresar jugadores
+		break;
+	
+	case (2):
+		//Jugar hiatos y diptongos
+		break;
+	
+	case (3):
+		//Jugar antónimos y sinónimos
+		break;
+	
+	case (4):
+		//Mostrar reporte de jugadores
+		break;
+	
+	case (5):
+		//Mostrar los 10 mejores jugadores
+		break;
+	
+	case (6):
+		//Salir del programa
+		exit(0);
+		break;
+	
+	default:
+		system("cls");
+		cout << " ***ALGÚN ERROR FATAL OCURRIÓ, ESTE CARTEL NUNCA DEBIÓ MOSTRARSE***\n\n"
+		"                LA EJECUCIÓN HA TERMINADO AUTOMÁTICAMENTE";
+		exit(1);
+		break;
+	}
+
+	return 0;
+}
+
+int MenuPrincipal() {
+	int Eleccion = 0;//Almacena la elección del usuario para el menú y es retornado en caso de cumplirse parametros de entrada
+	bool RespuestaValida = false;//Almacena estado de validez de datos ingresados
+	string InfoIngresada;//Almacena todos los datos ingresados para efectuar validación de los mismos
+	
 	cout <<	"****************************************************************\n*";
 	cout.width(39);
 	cout << right << "Menú Principal";
@@ -23,25 +71,40 @@ int main() {
 	"5. Los 10 mejores jugadores\n"
 	"6. Salir del programa\n\n"
 	"Digite un número para acceder a la opción: ";
-
     do {
-        cin >> EleccionMenu;
-        RespuestaInvalida = (CinFail() || EleccionMenu>6 || EleccionMenu<1);
-        if (RespuestaInvalida) {
+        getline(cin, InfoIngresada, '\n');
+		if (ValidarCaracterNumerico(InfoIngresada[0])) {
+			int Eleccion = ConvertirChar(InfoIngresada[0]);
+			RespuestaValida = (InfoIngresada.length() == 1 && 1 <= Eleccion && Eleccion <= 6);
+		} else {
+			RespuestaValida = false;
+		}//Fin if-else de validaciones de parámetros de ingreso
+		
+        if (!RespuestaValida) {
             cout << "\n\n   ***ERROR, DATOS INGRESADOS INVÁLIDOS***\n\n"
             "Digite un número para acceder a la opción: ";
-        }
-    } while(RespuestaInvalida);
-    cout << "\n\n";
-    return 0;
-}
+        }//Fin if para mostar mensaje de error
+    } while(!RespuestaValida);
+    return Eleccion;
+}//Fin función que imprime el menú y retorna elección del usuario
 
-bool CinFail() {
-    if (cin.fail()) {
-        cin.clear();
-        cin.ignore();
-        fflush(stdin);
-        return true;
-    } 
-    return false;
-}
+bool ValidarCaracterNumerico(char Caracter) {
+	const int CantCaracNumericos = 10;//Cantidad total de posibles caracteres númericos
+	char ValidarCaracterNumerico[CantCaracNumericos] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};//Arreglo de posibles caracteres numéricos
+	for (int Indice = 0 ; Indice < CantCaracNumericos ; Indice++) {
+		if (Caracter == ValidarCaracterNumerico[Indice]) {
+			return true;
+		}//Fin if que compara al caracter con el elemento del arreglo de posibles caracteres numéricos
+	}//Fin for que recorre los elementos del Arreglo de posibles caracteres numéricos
+	return false;
+}//Fin función que valida que el argumento sea un caracter numérico mediante búsqueda lineal de una coincidencia con él en el arreglo de posibles caracteres numéricos
+
+int ConvertirChar(char Caracter) {
+	const int CantCaracNumericos = 10;//Cantidad total de posibles caracteres númericos
+	char ValidarCaracterNumerico[CantCaracNumericos] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};//Arreglo de posibles caracteres numéricos
+	for (int Indice = 0 ; Indice < CantCaracNumericos ; Indice++) {
+		if (Caracter == ValidarCaracterNumerico[Indice]) {
+			return Indice;//Se retorna el valor numérico del caracter númerico de ser el caso
+		}//Fin if que compara al caracter con el elemento del arreglo de posibles caracteres numéricos
+	}//Fin for que recorre los elementos del Arreglo de posibles caracteres numéricos
+}//Fin función que recibe un caracter numérico y retorna su quivalente en tipo int
